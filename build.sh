a1e46439b7eb4893450571dd4e940f2df60c7871
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2020-09-08 21:57:57 -0300

    disabling squash

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 tags="7.4.10 7.4 7"
 for t in fpm cli; do
     ot="carlositline/php-full:$t-alpine"
-    docker build --squash -t $ot 7.4/alpine/$t || exit 1
+    docker build -t $ot 7.4/alpine/$t || exit 1
     for tag in $tags; do
         docker tag $ot "carlositline/php-full:$tag-$t-alpine" || exit 1
         docker push "carlositline/php-full:$tag-$t-alpine" || exit 1
