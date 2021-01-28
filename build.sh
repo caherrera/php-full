555514dd877b77cbcd61c971e385085dfb2f0cf6
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2021-01-28 11:01:52 -0300

    fix missing extensions

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@
 #docker build --squash -t carlositline/php-full:7.4.10-fpm-alpine 7.4/alpine/fpm
 #docker build --squash -t carlositline/php-full:7.4.10-cli-alpine 7.4/alpine/cli
 
-tags="7.4.10 7.4 7"
-for t in fpm cli; do
-    ot="carlositline/php-full:$t-alpine"
-    docker build -t $ot 7.4/alpine/$t || exit 1
-    for tag in $tags; do
-        docker tag $ot "carlositline/php-full:$tag-$t-alpine" || exit 1
-        docker push "carlositline/php-full:$tag-$t-alpine" || exit 1
-    done
-    docker push $ot || exit 1
-done
+#tags="7.4.14 7.4 7"
+#for o in alpine buster; do
+#  for t in fpm cli; do
+#      ot="carlositline/php-full:$t-$o"
+#      docker build -t $ot 7.4/$o/$t || exit 1
+#      for tag in $tags; do
+#          docker tag $ot "carlositline/php-full:$tag-$t-$o" || exit 1
+#          docker push "carlositline/php-full:$tag-$t-$o" || exit 1
+#      done
+#      docker push $ot || exit 1
+#  done
+#done
+echo
