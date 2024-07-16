f527e5235787b6503d3a21aafe0512fe47ddb506
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2024-07-16 14:38:17 -0400

    build

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-#docker build --squash -t carlositline/php-full:7.4.10-fpm-alpine 7.4/alpine/fpm
-#docker build --squash -t carlositline/php-full:7.4.10-cli-alpine 7.4/alpine/cli
+docker_prefix="docker buildx build --platform linux/amd64,linux/arm64,linux/arm64/v8,linux/arm/v7,linux/arm/v6,linux/386 --push "
 
-#tags="7.4.14 7.4 7"
-#for o in alpine buster; do
-#  for t in fpm cli; do
-#      ot="carlositline/php-full:$t-$o"
-#      docker build -t $ot 7.4/$o/$t || exit 1
-#      for tag in $tags; do
-#          docker tag $ot "carlositline/php-full:$tag-$t-$o" || exit 1
-#          docker push "carlositline/php-full:$tag-$t-$o" || exit 1
-#      done
-#      docker push $ot || exit 1
-#  done
-#done
+for df in `find . -name Dockerfile`; do  
+    d=$(dirname $df)
+    tag=$(echo $df | cut -d'/' -f2)
+    os=$(echo $df | cut -d'/' -f3)
+    svc=$(echo $df | cut -d'/' -f4)
+    t1="carlositline/php-full:$tag-$svc-$os"
+    t2="carlositline/php-full:$tag-$os"
+    echo $docker_prefix -t $t1 -t $t2 $d
+done
 echo
